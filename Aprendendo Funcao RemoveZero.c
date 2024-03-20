417cea3c20c84f906be20117d9d765a672f5654c
viniciusVPC <viniciuspereira2206@gmail.com>
2024-03-20 20:37:03 -0300

    Initial commit

---
+++ Aprendendo Funcao RemoveZero.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int RemoveZero (int v[], int n)
+{
+    int i = 0, j;
+    for(j = 0; j < n; j++)
+        if(v[j] != 0){
+            v[i] = v[j];
+            i += 1;
+
+        printf("%d\t", v[j]);
+        }
+        printf("\n");
+    return i;
+}
+
+int main(){
+
+    printf("Ola Seja bem vindo ao novo Criador de Vetores 3000!!!\n\n");
+    printf("Sem mais delongas, insira o tamanho que voce quer que o seu vetor tenha:");
+    int tamanhoVetor;
+    scanf("%d", &tamanhoVetor);
+    int *vetor;
+    vetor = (int *)malloc(tamanhoVetor * sizeof(int));
+    bool temZeros = false;
+    for(int i = 0; i < tamanhoVetor; i++){
+        printf("Insira o valor do %do elemento do seu array: ", i+1);
+        scanf("%d", &vetor[i]);
+        if(vetor[i] == 0) temZeros = true;
+    }
+
+    printf("Legal, agora vamos imprimir o seu vetor!\n\n");
+    for(int i = 0; i < tamanhoVetor; i++){
+        printf("%d\t", vetor[i]);
+    }
+
+    if(temZeros){
+        printf("\n\nOpa, notamos que o seu vetor possui alguns zeros!\nSegundo a nova lei universal, zeros em vetores sao proibidos!\n");
+        printf("Vamos remover esses zeros pra voce!\n\n");
+        printf("Pronto! Olha como o seu vetor esta bonitinho agora: \n\n");
+        int x = RemoveZero(vetor, tamanhoVetor);
+    }
+    else{
+        printf("\n\nQue belo vetor!");
+    }
+    free(vetor);
+    return 0;
+}
